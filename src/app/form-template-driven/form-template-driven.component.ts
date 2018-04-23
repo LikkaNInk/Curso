1ec8d3a46bd3907d770331b52aded6b0dffa4088
Aline <likkaninkli@gmail.com>
2018-04-23 14:09:21 -0300

    curso

---
+++ src/app/form-template-driven/form-template-driven.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-template-driven',
+  templateUrl: './form-template-driven.component.html',
+  styleUrls: ['./form-template-driven.component.css']
+})
+export class FormTemplateDrivenComponent implements OnInit {
+
+
+  titulo: string = "cadastro de cursos";
+  
+  curso: any = {
+    nome: null,
+    id: null
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(Form){
+
+    console.log(Form.value);
+  }
+}
